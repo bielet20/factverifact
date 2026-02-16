60b3acae7a4d44c7816fd0964945e9ec2b10e084
bielet20 <bielrivero@gmail.com>
2026-02-16 20:43:30 +0100

    chore: cleanup temporary files and finalize data/ folder structure

--- temp-init-schema.js
+++
@@ -1,9 +0,0 @@
-const db = require('./database.js');
-console.log('Database schema initialization triggered.');
-setTimeout(() => {
-    db.close((err) => {
-        if (err) console.error(err.message);
-        console.log('Database connection closed.');
-        process.exit(0);
-    });
-}, 2000);
